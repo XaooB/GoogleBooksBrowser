4ec5774e2512c025670400d6a00357466498fd0f
XaooB <prawo123@interia.pl>
2019-06-27 15:56:07 +0200

    replaced $ sign with &

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -4,7 +4,7 @@ export default async options => {
   const { query, index } = options;
 
   const response = await axios.get(
-    `https://www.googleapis.com/books/v1/volumes?${query}$key=AIzaSyAu-a5S6Uoyh6ie-PWGYqJPRKlHWJSQlmI`,
+    `https://www.googleapis.com/books/v1/volumes?${query}&key=AIzaSyAu-a5S6Uoyh6ie-PWGYqJPRKlHWJSQlmI`,
     {
       params: {
         startIndex: index
