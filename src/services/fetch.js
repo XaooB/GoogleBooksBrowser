4e103c9e71ed4f55eb1504278557df2bf2fa1be0
XaooB <prawo123@interia.pl>
2019-06-27 15:53:29 +0200

    update

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -4,11 +4,10 @@ export default async options => {
   const { query, index } = options;
 
   const response = await axios.get(
-    `https://www.googleapis.com/books/v1/volumes?${query}`,
+    `https://www.googleapis.com/books/v1/volumes?${query}$key=AIzaSyAu-a5S6Uoyh6ie-PWGYqJPRKlHWJSQlmI`,
     {
       params: {
-        startIndex: index,
-        key: process.env.key
+        startIndex: index
       }
     }
   );
